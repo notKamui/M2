4313e6037747466c64fa15a3753c51294c367732
notKamui <teillard.jimmy@outlook.fr>
2022-10-10 18:00:18 +0200

    Update

---
+++ JEE/tp4/src/main/java/fr/uge/jee/springmvc/rectangle/RectangleController.java
@@ -0,0 +1,31 @@
+package fr.uge.jee.springmvc.rectangle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/rectangle")
+public class RectangleController {
+
+    @GetMapping
+    public String rectangle(@ModelAttribute("rectangle") RectangleForm form) {
+        return "rectangle-form";
+    }
+
+    @PostMapping
+    public String rectangle(@Valid @ModelAttribute("rectangle") RectangleForm form, BindingResult binding, Model model) {
+        if (binding.hasErrors()) {
+            model.addAttribute("invalid", true);
+            return "rectangle-form";
+        }
+        model.addAttribute("area", form.getWidth() * form.getHeight());
+        return "rectangle-result";
+    }
+}
---
+++ JEE/tp4/src/main/java/fr/uge/jee/springmvc/rectangle/RectangleForm.java
@@ -0,0 +1,31 @@
+package fr.uge.jee.springmvc.rectangle;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+public class RectangleForm {
+
+    @NotNull(message = "Width is required")
+    @PositiveOrZero(message = "Width must be positive or zero")
+    private int width;
+
+    @NotNull(message = "Height is required")
+    @PositiveOrZero(message = "Height must be positive or zero")
+    private int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
